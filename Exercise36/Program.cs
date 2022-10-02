07ecc1df97f86a5a787817174de68aeb06ba83e1
Sergey <bibert.shmibert@gmail.com>
2022-10-02 22:14:07 +0400

    add exercise 36

---
+++ Exercise36/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using myLib;
+
+void sumOfEvenIndex()
+{
+    Random randomSize = new Random();
+    int size = randomSize.Next(5, 11);
+    int[] numbers = new int[size];
+    ArrayMethods.fillArray(numbers, -1000, 1000);
+    int summ = 0;
+    for(int i = 1; i < numbers.Length; i+=2)
+    {
+        if(i % 2 != 0) 
+        {
+            summ+= numbers[i];
+        }
+    }
+    ArrayMethods.printArray(numbers);
+    Console.WriteLine(summ);
+}
+
+sumOfEvenIndex();
