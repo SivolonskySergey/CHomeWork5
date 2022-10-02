f247246884b276214ac5fd0561c1fab4893b38df
Sergey <bibert.shmibert@gmail.com>
2022-10-02 14:53:23 +0400

    add exercise 34

--- Exercise34/Program.cs
+++ Exercise34/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void showArray(int[] arr) 
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+};
+
+
+void fillArray(int[] arr, int minValue, int maxValue) 
+{
+    maxValue++;
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        arr[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void amountOfEven()
+{
+    int size = 3;
+    int[] numbers = new int[size];
+    fillArray(numbers, 0, 10000);
+    int count = 0;
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        if(numbers[i] % 2 == 0)
+        {
+            count+=1;
+        }
+    }
+    showArray(numbers);
+    Console.WriteLine($"Четных чисел в массиве - {count}");
+}
+
+amountOfEven();
